970ef895c8d83521c9f185feb724a801232a8c93
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-10-10 16:12:54 +0300

    Implemented browser service

---
+++ TestmonitorProject/Services/UI/BrowserService.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace TestmonitorProject.Services.UI;
+
+public class BrowserService
+{
+    public IWebDriver WebDriver { get; }
+
+    public BrowserService(string browserName)
+    {
+        switch (browserName.ToLower())
+        {
+            case "chrome" : var chromeOptions = new DriverOptionsProvider().GetChromeDriverOptions();
+                WebDriver = new ChromeDriver(chromeOptions);
+                break;
+            case "firefox" : var FirefoxOptions = new DriverOptionsProvider().GetFirefoxDriverOptions();
+                WebDriver = new FirefoxDriver(FirefoxOptions);
+                break;
+            default: throw new Exception("Incorrect Browser Name.");
+        }
+        
+        WebDriver.Manage().Window.Maximize();
+        WebDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
+    }
+}
