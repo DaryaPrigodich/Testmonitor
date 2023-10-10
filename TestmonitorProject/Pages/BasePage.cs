4cecbe82100b5acd7a2274688287f443e5182895
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-10-11 00:34:26 +0300

    Implemented base page

---
+++ TestmonitorProject/Pages/BasePage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+
+namespace TestmonitorProject.Pages;
+
+public abstract class BasePage
+{
+    protected static IWebDriver Driver = null!;
+
+    protected abstract void OpenPage();
+
+    protected BasePage(IWebDriver driver, bool openPageByUrl)
+    {
+        Driver = driver;
+
+        if (openPageByUrl)
+        {
+            OpenPage();
+        }
+    }
+}
