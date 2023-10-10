067d7322b7d5da2a759dc4bf8b67aaab692abc7f
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-10-10 22:28:02 +0300

    Implemented models for project response deserialization

---
+++ TestmonitorProject/Models/Endpoints.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TestmonitorProject.Models;
+
+public record Endpoints
+{
+    [JsonPropertyName("show")]
+    public string Show { get; set; }
+    [JsonPropertyName("update")]
+    public string Update { get; set; }
+    [JsonPropertyName("archive")]
+    public string Archive { get; set; }
+    [JsonPropertyName("unarchive")]
+    public string Unarchive { get; set; }
+    [JsonPropertyName("members")]
+    public string Members { get; set; }
+    [JsonPropertyName("non-members")]
+    public string NonMembers { get; set; }
+}
---
+++ TestmonitorProject/Models/ProjectResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TestmonitorProject.Models;
+
+public record ProjectResponse
+{
+    [JsonPropertyName("data")]
+    public Data Data { get; set; }
+}
