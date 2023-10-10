2708c137596b063705686d4089ebc0b26d1efeef
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-10-10 16:16:30 +0300

    Implemented wait service to improve reliability

---
+++ TestmonitorProject/Services/UI/WaitService.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using TestmonitorProject.Configuration;
+
+namespace TestmonitorProject.Services.UI;
+
+public class WaitService
+{
+    private readonly IWebDriver _driver;
+    private readonly WebDriverWait _waitService;
+
+    public WaitService(IWebDriver driver)
+    {
+        _driver = driver;
+        _waitService = new WebDriverWait(_driver, TimeSpan.FromSeconds(Configurator.AppSettings.WaitTimeout));
+    }
+}
