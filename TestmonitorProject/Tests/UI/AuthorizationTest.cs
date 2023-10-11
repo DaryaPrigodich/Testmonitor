270ef387dbb7723e7dd869ba19322b2749260e2e
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-10-11 16:16:39 +0300

    Implemented invalid authorization test case

---
+++ TestmonitorProject/Tests/UI/AuthorizationTest.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace TestmonitorProject.Tests.UI;
+
+public class AuthorizationTest: BaseUiTest
+{
+    [Test]
+    [TestCase("invalid@email", "123", "These credentials do not match our records.")]
+    public void AuthorizationUsingInvalidCredentials(string username, string password, string errorMessage)
+    {
+        var loginErrorMessage = LoginSteps
+            .LoginWithInvalidCredentials(username,password);
+
+        loginErrorMessage.Should().Be(errorMessage, "User with invalid credentials has an access to the app.");
+    }
+}
