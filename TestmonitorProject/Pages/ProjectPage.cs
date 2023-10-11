5e31e5354b6e37ddfd21da96c15a62a409b846ec
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-10-11 14:12:45 +0300

    Implemented test suite page and related methods

--- TestmonitorProject/Pages/ProjectPage.cs
+++ TestmonitorProject/Pages/ProjectPage.cs
@@ -9,6 +9,7 @@ public class ProjectPage : BasePage
     private string Endpoint(string projectId) => $"{projectId}";
     
     private UiElement Requirements => new (Driver, By.XPath("//span[contains(text(),'requirements')]"));
+    private UiElement TestCases => new (Driver, By.XPath("//span[contains(text(),'test cases')]"));
 
     public ProjectPage(IWebDriver driver, bool openPageByUrl, string projectId) : base(driver, openPageByUrl)
     {
@@ -28,5 +29,12 @@ public class ProjectPage : BasePage
         Requirements.Click();
 
         return new RequirementsPage(Driver);
+    }
+    
+    public TestSuitesPage OpenProjectTestCases()
+    {
+        TestCases.Click();
+
+        return new TestSuitesPage(Driver);
     }
 }
---
+++ TestmonitorProject/Pages/TestSuitesPage.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using TestmonitorProject.Configuration;
+using TestmonitorProject.Wrappers;
+
+namespace TestmonitorProject.Pages;
+
+public class TestSuitesPage : BasePage
+{
+    private string Endpoint(string projectId) => $"{projectId}/design/test-suites";
+
+    private UiElement AddTestSuiteButton => new(Driver, By.XPath("//*[contains(text(),'Add Test Suite')]"));
+    private UiElement TestSuiteNameInput => new(Driver, By.XPath("//*[@name='name']")); 
+    private UiElement CreateTestSuiteButton => new(Driver, By.XPath("//*[@type='submit']"));
+    private UiElement SuccessCreatedMessage => new(Driver, By.XPath("//*[@role='alert']//*[contains(text(),'suite')]"));
+    
+    public TestSuitesPage(IWebDriver driver, bool openPageByUrl) : base(driver, openPageByUrl)
+    {
+    }
+
+    public TestSuitesPage(IWebDriver driver) : base(driver, false)
+    {
+    }
+
+    protected override void OpenPage()
+    {
+        Driver.Navigate().GoToUrl(Configurator.AppSettings.UiUrl + Endpoint);
+    }
+
+    public TestSuitesPage ClickAddTestSuiteButton()
+    {
+        AddTestSuiteButton.Click();
+
+        return this;
+    }
+ 
+    public bool CreateTestSuite()
+    {
+        var testSuiteName = new Bogus.Faker().Lorem.Word();
+
+        TestSuiteNameInput.SendKeys(testSuiteName);
+        CreateTestSuiteButton.Click();
+
+        return SuccessCreatedMessage.Displayed;
+    }
+}
